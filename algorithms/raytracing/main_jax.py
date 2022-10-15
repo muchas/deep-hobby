1008c70ae614fa128d107d37842f6f96c6e8fd34
Sławomir Mucha <mucha.slawomir@gmail.com>
2022-10-15 19:31:25 +0100

    Remove custom record representation, use more idiomatic Jax constructs

--- algorithms/raytracing/main_jax.py
+++ algorithms/raytracing/main_jax.py
@@ -2,14 +2,12 @@ import math
 import random as pyrandom
 import jax.numpy as jnp
 
-from jax import jit, vmap, random, devices, device_put
-
-from jax import lax
+from jax import jit, lax, vmap, random, devices, device_put, tree_map, xla_computation
 
 from collections import namedtuple
 from cached_property import cached_property
 from functools import partial
-from typing import Optional, List, Tuple
+from typing import Optional, List, Tuple, NamedTuple
 from tqdm import tqdm
 
 
@@ -45,6 +43,13 @@ def reflect(ray_direction: jnp.array, normal: jnp.array) -> jnp.array:
     return ray_direction - 2 * jnp.dot(normal, ray_direction) * normal
 
 
+def refract(vector: jnp.array, normal: jnp.array, refraction_ratio: float) -> jnp.array:
+    cos_theta = min(jnp.dot(-vector, normal), 1.0)
+    r_out_perp = refraction_ratio * (vector + cos_theta * normal)
+    r_out_parallel = -jnp.sqrt(jnp.abs(1.0 - jnp.dot(r_out_perp, r_out_perp))) * normal
+    return r_out_perp + r_out_parallel
+
+
 class Ray:
     def __init__(self, origin: jnp.array, direction: jnp.array):
         self.origin = origin
@@ -54,18 +59,45 @@ class Ray:
         return self.origin + t * self.direction
 
 
+class ScatteredRay:
+    def __init__(self, ray: Ray, attenuation: jnp.array):
+        self.ray = ray
+        self.attenuation = attenuation
+
+
 class Hittable:
     def hit(self, prng_key, ray: Ray, t_min: float, t_max: float) -> float:
         raise NotImplemented()
 
 
-class Sphere(Hittable):
+class HitRecordV2(NamedTuple):
+    was_hit: bool
+    ray_direction: jnp.array
+    t: float
+    hit_point: jnp.array
+    normal: jnp.array
+    scattered_ray_direction: jnp.array
+    scattered_ray_attenuation: jnp.array
+
+
+MISSED_HIT_RECORD = HitRecordV2(
+    was_hit=jnp.array([False], dtype=jnp.int32),
+    ray_direction=vector(0, 0, 0),
+    t=jnp.inf,
+    hit_point=vector(0, 0, 0),
+    normal=vector(0, 0, 0),
+    scattered_ray_direction=vector(0, 0, 0),
+    scattered_ray_attenuation=vector(0, 0, 0),
+)       
+
+
+class SphereV2(Hittable):
     def __init__(self, origin: jnp.array, radius: float, material):
         self.origin = origin
         self.radius = radius
         self.material = material
 
-    def hit(self, prng_key, ray: Ray, t_min: float, t_max: float) -> float:
+    def hit(self, prng_key, ray: Ray, t_min: float, t_max: float) -> HitRecordV2:
         material = self.material
         sphere_origin = self.origin
         sphere_radius = self.radius
@@ -91,15 +123,15 @@ class Sphere(Hittable):
 
             scattered_ray = material.scatter(prng_key, hitpoint, normal, ray.direction)
 
-            return HitRecord.create(
+            return HitRecordV2(
+                was_hit=jnp.array([True], dtype=jnp.int32),
                 ray_direction=ray.direction,
                 t=t,
                 hit_point=hitpoint,
                 normal=normal,
-                scattered_ray_direction=scattered_ray.direction,
-            ).data
-
-        missed_hit_record = HitRecord.missed().data
+                scattered_ray_direction=scattered_ray.ray.direction,
+                scattered_ray_attenuation=scattered_ray.attenuation,
+            )
 
         @jit
         def determine_hitpoint(delta):
@@ -110,130 +142,30 @@ class Sphere(Hittable):
                 (t_min < t1) & (t1 < t2),
                 lambda: hit_record(t1),
                 lambda: lax.cond(
-                    t_min < t2, lambda: hit_record(t2), lambda: missed_hit_record
+                    t_min < t2, lambda: hit_record(t2), lambda: MISSED_HIT_RECORD
                 ),
             )
 
         return lax.cond(
             discriminant > 0,
             lambda: determine_hitpoint(discriminant),
-            lambda: missed_hit_record,
+            lambda: MISSED_HIT_RECORD,
         )
 
 
-RecordField = ("RecordField", "start_index length")
-
-# dataclass
-# todo: add ser/deser methods to keep things inside jax arrays
-class HitRecord:
-
-    # _WAS_HIT_FIELD = RecordField(start_index=0, length=1)
-    # _T_FIELD = RecordField(start_index=_WAS_HIT_FIELD.start_index)
-
-    _WAS_HIT_LENGTH = 1
-    _RAY_DIRECTION_LENGTH = 3
-    _T_LENGTH = 1
-    _HIT_POINT_LENGTH = 3
-    _NORMAL_LENGTH = 3
-    _SCATTERED_RAY_DIRECTION_LENGTH = 3
-
-    _WAS_HIT_INDEX = 0
-    _RAY_DIRECTION_INDEX = _WAS_HIT_INDEX + _WAS_HIT_LENGTH
-    T_INDEX = _RAY_DIRECTION_INDEX + _RAY_DIRECTION_LENGTH
-    _HIT_POINT_INDEX = T_INDEX + _T_LENGTH
-    _NORMAL_INDEX = _HIT_POINT_INDEX + _HIT_POINT_LENGTH
-    _SCATTERED_RAY_DIRECTION_INDEX = _NORMAL_INDEX + _SCATTERED_RAY_DIRECTION_LENGTH
-
-    TOTAL_LENGTH = (
-        _WAS_HIT_LENGTH
-        + _RAY_DIRECTION_LENGTH
-        + _T_LENGTH
-        + _HIT_POINT_LENGTH
-        + _NORMAL_LENGTH
-        + _SCATTERED_RAY_DIRECTION_LENGTH
-    )
-
-    def __init__(self, data: jnp.array):
-        self.data = data
-
-    @cached_property
-    def was_hit(self):
-        return self.data[self._WAS_HIT_INDEX] > 0
-
-    @cached_property
-    def ray_direction(self):
-        return self.data[
-            self._RAY_DIRECTION_INDEX : self._RAY_DIRECTION_INDEX
-            + self._RAY_DIRECTION_LENGTH
-        ]
-
-    @cached_property
-    def t(self):
-        return self.data[self.T_INDEX]
-
-    @cached_property
-    def hit_point(self):
-        return self.data[
-            self._HIT_POINT_INDEX : self._HIT_POINT_INDEX + self._HIT_POINT_LENGTH
-        ]
-
-    @cached_property
-    def normal(self):
-        return self.data[self._NORMAL_INDEX : self._NORMAL_INDEX + self._NORMAL_LENGTH]
-
-    @cached_property
-    def scattered_ray_direction(self):
-        return self.data[
-            self._SCATTERED_RAY_DIRECTION_INDEX : self._SCATTERED_RAY_DIRECTION_INDEX
-            + self._SCATTERED_RAY_DIRECTION_LENGTH
-        ]
-
-    @cached_property
-    def scattered_ray(self) -> Ray:
-        return Ray(origin=self.hit_point, direction=self.scattered_ray_direction)
-
-    @staticmethod
-    def create(**kwargs):
-        return HitRecord._create(was_hit=True, **kwargs)
-
-    @staticmethod
-    def missed():
-        return HitRecord._create(
-            was_hit=False,
-            ray_direction=vector(0, 0, 0),
-            t=jnp.inf,
-            hit_point=vector(0, 0, 0),
-            normal=vector(0, 0, 0),
-            scattered_ray_direction=vector(0, 0, 0),
-        )
-
-    @staticmethod
-    def _create(was_hit, ray_direction, t, hit_point, normal, scattered_ray_direction):
-        was_hit_array = jnp.array((1.0,)) if was_hit else jnp.array((0.0,))
-        return HitRecord(
-            jnp.concatenate(
-                (
-                    was_hit_array,
-                    ray_direction,
-                    jnp.array((t,)),
-                    hit_point,
-                    normal,
-                    scattered_ray_direction,
-                )
-            )
-        )
-
-
-class World:
+class WorldV2:
     def __init__(self, objects: List[Hittable]):
         self.objects = objects
 
-    def hit(self, prng_key, ray: Ray, t_min: float, t_max: float) -> HitRecord:
-        hits = jnp.stack(
-            [hittable.hit(prng_key, ray, t_min, t_max) for hittable in self.objects]
-        )
-        min_t_object_index = jnp.argmin(hits[:, HitRecord.T_INDEX])
-        return HitRecord(data=hits[min_t_object_index, :])
+    def hit(self, prng_key, ray: Ray, t_min: float, t_max: float) -> HitRecordV2:
+        hit_records = [hittable.hit(prng_key, ray, t_min, t_max) for hittable in self.objects]
+        min_t_object_index = jnp.argmin(jnp.stack([hit_record.t for hit_record in hit_records]))
+
+        conditions = []
+        for obj_idx in range(len(hit_records)):
+            conditions.append(min_t_object_index == obj_idx)
+
+        return tree_map(lambda *x: jnp.select(conditions, x), *hit_records)
 
 
 class Camera:
@@ -300,76 +232,104 @@ class Camera:
 
 class Material:
     #  TODO(smucha): extract hit_point, normal and ray_direction into a separate primitive
-    def scatter(self, prgn_key, hit_point, normal, ray_direction) -> Ray:
-        raise NotImplemented()
-
-    def collect_color(self, scattered_ray_color: jnp.array) -> jnp.array:
+    def scatter(self, prgn_key, hit_point, normal, ray_direction) -> ScatteredRay:
         raise NotImplemented()
 
 
 class Lambertian(Material):
-    def scatter(self, prgn_key, hit_point, normal, _ray_direction) -> Ray:
+    def __init__(self, albedo):
+        self.albedo = albedo
+
+    def scatter(self, prgn_key, hit_point, normal, _ray_direction) -> ScatteredRay:
         random_unit_vector = unit(
             random.uniform(prgn_key, (3,), minval=-1.0, maxval=1.0)
         )
         scattered_ray_direction = hit_point + normal + random_unit_vector
-        return Ray(hit_point, scattered_ray_direction)
 
-    def collect_color(self, scattered_ray_color: jnp.array) -> jnp.array:
-        return 0.5 * scattered_ray_color
+        return ScatteredRay(
+            ray=Ray(hit_point, scattered_ray_direction), attenuation=self.albedo
+        )
 
 
 class Metal(Material):
-    def scatter(self, _prgn_key, hit_point, normal, ray_direction) -> Ray:
-        reflected_ray_direction = reflect(ray_direction, normal)
-        return Ray(hit_point, reflected_ray_direction)
+    def __init__(self, albedo, fuzziness: float = 0.0):
+        self.albedo = albedo
+        self.fuzziness = fuzziness
 
-    def collect_color(self, scattered_ray_color: jnp.array) -> jnp.array:
-        return 0.5 * scattered_ray_color
+    def scatter(self, prgn_key, hit_point, normal, ray_direction) -> ScatteredRay:
+        random_unit_vector = unit(
+            random.uniform(prgn_key, (3,), minval=-1.0, maxval=1.0)
+        )
+        reflected_ray_direction = (
+            reflect(ray_direction, normal) + self.fuzziness * random_unit_vector
+        )
+        return ScatteredRay(
+            ray=Ray(hit_point, reflected_ray_direction), attenuation=self.albedo
+        )
+
+
+class Dielectric(Material):
+    def __init__(self, index_of_refraction):
+        self.index_of_refraction = index_of_refraction
+
+    def scatter(self, prngn_key, hit_point, normal, ray_direction) -> ScatteredRay:
+        color = vector(1.0, 1.0, 1.0)
+
+        refraction_ratio = (
+            (1.0 / self.index_of_refraction)
+            if hit_record.front_face
+            else self.index_of_refraction
+        )
+
+        unit_direction = unit(ray.direction)
+        cos_theta = min(np.dot(-unit_direction, hit_record.normal), 1.0)
+        sin_theta = np.sqrt(1.0 - cos_theta * cos_theta)
+        cannot_refract = refraction_ratio * sin_theta > 1.0
+
+        if (
+            cannot_refract
+            or self.__reflectance(cos_theta, refraction_ratio) > random.random()
+        ):
+            target_direction = reflect(unit_direction, hit_record.normal)
+        else:
+            target_direction = refract(
+                unit_direction, hit_record.normal, refraction_ratio
+            )
+
+        return ScatteredRay(
+            ray=Ray(hit_record.point, target_direction), attenuation=color
+        )
+
+    def __reflectance(self, cosine: float, refraction_ratio: float) -> float:
+        # Schlick's approximation for reflectance
+        r_0 = (1.0 - refraction_ratio) / (1.0 + refraction_ratio)
+        r_0 = r_0 * r_0
+        return r_0 + (1 - r_0) * ((1 - cosine) ** 5)
 
 
 # it's important for this function to be jittable, so it could be executed on a target device as a single kernel
 def compute_ray_color(
     ray_direction, seed, ray_origin, world, max_scatter_steps: int = 4
 ) -> jnp.array:
-    material = Lambertian()
     ray = Ray(ray_origin, ray_direction)
 
-    @jit
-    def scatter(hit_record_data, prng_key):
-        hit_record = HitRecord(hit_record_data)
-        return world.hit(prng_key, hit_record.scattered_ray, 0.001, jnp.inf).data
-
     prng_key = random.PRNGKey(seed[0])
-    initial_hit_record = world.hit(prng_key, ray, 0.001, jnp.inf).data
-
-    all_hit_records = jnp.zeros((max_scatter_steps + 1, HitRecord.TOTAL_LENGTH))
-    all_hit_records = all_hit_records.at[0].set(initial_hit_record)
-
     prng_keys = random.split(prng_key, num=max_scatter_steps + 1)
 
-    def loop_body(input_tuple):
-        scatter_step, hit_record_data, all_hit_records = input_tuple
-        new_hit_record = lax.cond(
-            HitRecord(hit_record_data).was_hit,
-            lambda: scatter(hit_record_data, prng_keys[scatter_step]),
-            lambda: jnp.zeros((HitRecord.TOTAL_LENGTH,)),
-        )
-        return (
-            scatter_step + 1,
-            new_hit_record,
-            all_hit_records.at[scatter_step + 1].set(new_hit_record),
-        )
+    hit_record = world.hit(prng_key, ray, 0.001, jnp.inf)
+    all_hit_records = [hit_record]
 
-    _last_step_index, last_hit_record, all_hit_records = lax.while_loop(
-        lambda input_tuple: (input_tuple[0] < max_scatter_steps)
-        & (input_tuple[1][0] > 0),
-        loop_body,
-        (0, initial_hit_record, all_hit_records),
-    )
+    for scatter_step in range(max_scatter_steps):
+        new_hit_record = lax.cond(
+            jnp.all(hit_record.was_hit),
+            lambda: world.hit(prng_keys[scatter_step], Ray(hit_record.hit_point, hit_record.scattered_ray_direction), 0.001, jnp.inf),
+            lambda: MISSED_HIT_RECORD,
+        )
+        all_hit_records.append(new_hit_record)
+        hit_record = new_hit_record
 
     initial_color = lax.cond(
-        (all_hit_records[-1][0] > 0),
+        jnp.all(all_hit_records[-1].was_hit),
         lambda: color(0, 0, 0),
         lambda: background_color(ray),
     )
@@ -377,8 +337,8 @@ def compute_ray_color(
     final_color = initial_color
     for hit_record in all_hit_records[::-1]:
         final_color = lax.cond(
-            hit_record[0] > 0,
-            lambda c: material.collect_color(c),
+            jnp.all(hit_record.was_hit),
+            lambda c: hit_record.scattered_ray_attenuation * c,
             lambda c: c,
             final_color,
         )
@@ -436,17 +396,23 @@ def main():
     #    	Sphere(vector(0.0, -100.5, -1.0), 100)  # ground
     # ]
 
+    material_ground = Lambertian(vector(0.8, 0.8, 0.0))
+    material_center = Lambertian(vector(0.1, 0.2, 0.5))
+    # material_left = Dielectric(1.5)
+    material_right = Metal(vector(0.8, 0.6, 0.2), fuzziness=0.1)
+
     second_scene = [
-        Sphere(vector(-1, 0, -0.75), 0.2, Lambertian()),
-        Sphere(vector(1, 0, -1), 0.5, Lambertian()),
-        Sphere(vector(0, -0.2, -1.5), 0.25, Metal()),
-        Sphere(vector(0.0, -100.5, -1.0), 100, Lambertian()),  # ground
+        SphereV2(vector(-1, 0, -0.75), 0.2, material_center),
+        SphereV2(vector(1, 0, -1), 0.5, material_center),
+        SphereV2(vector(0, -0.2, -1.5), 0.25, material_right),
+        SphereV2(vector(-0.6, -0.2, -1.5), 0.25, material_right),
+        SphereV2(vector(0.0, -100.5, -1.0), 100, material_ground),  # ground
     ]
 
-    world = World(second_scene)
+    world = WorldV2(second_scene)
 
-    samples_per_pixel = 100
-    batch_size = 25
+    samples_per_pixel = 50
+    batch_size = 10
 
     pixel_colors = jnp.zeros(
         shape=(batch_size, image_height, image_width, channels_num), dtype=jnp.float32
@@ -477,6 +443,74 @@ def main():
     pixel_colors = jnp.sqrt(pixel_colors)  # gamma correction (gamma 2)
 
     print(write_ppm_image(pixel_colors))
+
+
+def save_computation_graph():
+    seed = 0
+    aspect_ratio = 16 / 9
+    image_width = 512
+    image_height = int(image_width / aspect_ratio)
+
+    viewport_height = 2
+    viewport_width = viewport_height * aspect_ratio
+    channels_num = 3
+
+    starting_key = random.PRNGKey(seed)
+    camera = Camera(
+        origin=vector(0, 0, 0),
+        image_size=(image_width, image_height),
+        viewport_size=(viewport_width, viewport_height),
+    )
+
+    # starting_scene = [
+    #   Sphere(vector(0, 0, -1), 0.5),
+    #       Sphere(vector(0.0, -100.5, -1.0), 100)  # ground
+    # ]
+
+    second_scene = [
+        Sphere(vector(-1, 0, -0.75), 0.2, Lambertian()),
+        Sphere(vector(1, 0, -1), 0.5, Lambertian()),
+        Sphere(vector(0, -0.2, -1.5), 0.25, Metal()),
+        Sphere(vector(-0.6, -0.2, -1.5), 0.25, Metal()),
+        Sphere(vector(0.0, -100.5, -1.0), 100, Lambertian()),  # ground
+    ]
+
+    world = World(second_scene)
+
+    samples_per_pixel = 50
+    batch_size = 10
+
+    pixel_colors = jnp.zeros(
+        shape=(batch_size, image_height, image_width, channels_num), dtype=jnp.float32
+    )
+
+    compute_ray_color_in_a_given_world = jit(
+        partial(compute_ray_color, ray_origin=camera.origin, world=world)
+    )
+    compute_ray_color_vectorized = vmap(vmap(vmap(compute_ray_color_in_a_given_world)))
+
+    batches_num = int(samples_per_pixel / batch_size) + math.ceil(
+        samples_per_pixel % batch_size
+    )
+    batch_prng_keys = random.split(starting_key, num=batches_num)
+
+    seeds = (
+        random.uniform(starting_key, (batch_size, image_height, image_width, 1)) * 1000
+    ).astype(int)
+
+    key = batch_prng_keys[0]
+    ray_directions = camera.get_ray_directions(
+        key, batch_size
+    )  # B x H x W x DIRECTION_DIM (3)
+
+    computation = xla_computation(compute_ray_color_vectorized)(ray_directions, seeds)
+
+    import pdb
+
+    pdb.set_trace()
+
+    with open("computation_graph.dot", "w") as f:
+        f.write(computation.as_hlo_dot_graph())
 
 
 main()
